c4acae72b4eb7b77c292f2b4a0911d7d56329f3d
Jorge <jorgeaparicio79@gmail.com>
2021-10-23 18:01:38 +0200

    Commit Jorge --> RestControllers

--- src/main/java/com/example/proyectotesting/controller/rest/DirectionRestController.java
+++ src/main/java/com/example/proyectotesting/controller/rest/DirectionRestController.java
@@ -1,7 +1,14 @@
 package com.example.proyectotesting.controller.rest;
 
+import com.example.proyectotesting.service.DirectionService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DirectionRestController {
+
+    private DirectionService directionService;
+
+    public DirectionRestController (DirectionService directionService) {
+        this.directionService = directionService;
+    }
 }
--- src/main/java/com/example/proyectotesting/controller/rest/ManufacturerRestController.java
+++ src/main/java/com/example/proyectotesting/controller/rest/ManufacturerRestController.java
@@ -1,7 +1,15 @@
 package com.example.proyectotesting.controller.rest;
 
+import com.example.proyectotesting.service.ManufacturerService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ManufacturerRestController {
+
+    private ManufacturerService manufacturerService;
+
+    public ManufacturerRestController (ManufacturerService manufacturerService) {
+        this.manufacturerService = manufacturerService;
+    }
+
 }
