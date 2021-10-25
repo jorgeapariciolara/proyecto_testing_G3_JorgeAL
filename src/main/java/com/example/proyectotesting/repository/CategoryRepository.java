241e606ef892e6e491a48cc861feb0ac7658313e
alvarogarcia94 <alvarogb.barcelona94@gmail.com>
2021-10-25 13:46:33 +0200

    #7. CategoryServiceImpl

--- src/main/java/com/example/proyectotesting/repository/CategoryRepository.java
+++ src/main/java/com/example/proyectotesting/repository/CategoryRepository.java
@@ -4,9 +4,12 @@ import com.example.proyectotesting.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
-	
-	
+
+    Optional<Category> findByColor(String color);
+
 
 }
--- src/main/java/com/example/proyectotesting/service/CategoryServiceImpl.java
+++ src/main/java/com/example/proyectotesting/service/CategoryServiceImpl.java
@@ -1,21 +1,38 @@
 package com.example.proyectotesting.service;
 
 import com.example.proyectotesting.entities.Category;
+import com.example.proyectotesting.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    private CategoryRepository repository; // dependencia
+
+    public CategoryServiceImpl(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+
     @Override
     public List<Category> findAll() {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public Optional<Category> findOne(Long id) {
         return Optional.empty();
+    }
+
+    @Override
+    public Optional<Category> findOne(String color) {
+        if (color == null)
+            return Optional.empty();
+
+        return repository.findByColor(color);
     }
 
     @Override
