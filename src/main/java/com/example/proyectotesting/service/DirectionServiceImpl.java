de9ee190ea030faf5b375101be51d9623cdbcd5d
Miguel Ángel <mangelc13@gmail.com>
2021-10-26 09:10:46 +0200

    #4 Métodos de DirectionServiceImpl implementados

--- src/main/java/com/example/proyectotesting/service/DirectionServiceImpl.java
+++ src/main/java/com/example/proyectotesting/service/DirectionServiceImpl.java
@@ -1,21 +1,21 @@
 package com.example.proyectotesting.service;
 
 import com.example.proyectotesting.entities.Direction;
+import com.example.proyectotesting.repository.DirectionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class DirectionServiceImpl implements DirectionService{
+
+    private DirectionRepository repository;
+
     @Override
     public List<Direction> findAll() {
         return null;
-    }
-
-    @Override
-    public Optional<Direction> findOne(Long id) {
-        return Optional.empty();
     }
 
     @Override
@@ -31,5 +31,13 @@ public class DirectionServiceImpl implements DirectionService{
     @Override
     public boolean deleteById(Long id) {
         return false;
+    }
+
+    @Override
+    public List<Direction> findByCityAndCountry(String city, String country) {
+        List<Direction> result = new ArrayList<>();
+        if (city == null || country == null)
+            return result;
+        return repository.findByCityAndCountry(city,country);
     }
 }
