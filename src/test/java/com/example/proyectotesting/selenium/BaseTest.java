c212515dc1ae3b8d1bcd2245beaccf109eee6a24
Jorge <jorgeaparicio79@gmail.com>
2021-11-30 09:31:16 +0100

    Ajuste pom

--- src/test/java/com/example/proyectotesting/selenium/BaseTest.java
+++ src/test/java/com/example/proyectotesting/selenium/BaseTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
